c974908b2e4eeb9445943ebf251ed16384289fd3
Fabiano Dicheti <117326271+FabianoDicheti@users.noreply.github.com>
2025-08-26 14:37:59 -0300

    Update cadeira_preto.py
    exercícios

--- cadeira_preto.py
+++ cadeira_preto.py
@@ -1,4 +1,5 @@
 
+################## -> coloque o cabeçalho do arquivo
 
 #Em python o que é um cabeçalho do arquivo python,
 #para que ele serve, qual a diferença do cabeçalho
@@ -15,3 +16,61 @@
 #entre comentários e docstrings, mas para o usuário a diferença é marcante, pois uma docstring
 #não é ignorada pelo interpretador, mas passa a fazer parte do objeto sobre o qual ela foi definida,
 #podendo ser acessada através do help.
+
+
+
+
+######################################################################
+
+# implemente as funções abaixo e coloque as docstrings
+
+def maximo(nums):
+    """oque faz
+    oque recebe
+    oque retorna"""
+    # TODO: percorra a lista guardando o maior atual
+    ...
+
+def e_par(n: int) -> bool:
+    """ ... """
+    # TODO: retorne se n é par
+    ...
+def fatorial(n: int) -> int:
+    """   ...  """
+    # TODO: implemente de forma iterativa (sem recursão)
+    ...
+import re
+
+def limpa_texto(s: str) -> str:
+    """..."""
+    # TODO: converta s para minúsculo e remova pontuações como ,.;:!?'"()-_
+    ...
+
+def conta_vogais(s: str) -> int:
+    """....."""
+    # TODO: conte as vogais simples
+    ...
+
+def palindromo(s: str) -> bool:
+    """..."""
+    # TODO: normalizar (minúsculo, remover não alfanumérico) e comparar com o reverso
+    ...
+
+def total_por_vendedor(vendas):
+    """
+    executa......
+    Recebe: lista de tuplas (nome, valor).
+    Retorna: dict {nome: soma_valores}
+    """
+    # TODO: inicialize um dict e vá somando
+    ...
+
+def melhor_vendedor(totais: dict):
+    """
+    ...
+    Recebe ...
+    Retorna (nome, total) com o maior total.
+    Se dict vazio, levante ValueError.
+    """
+    # TODO: encontre o par com maior total (sem ordenar a lista inteira)
+    ...
