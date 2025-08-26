db0d7b06a3a414a4d555a107e66c1248bbf8d6fe
patrickfernandesconceicao <patrickfernandesconceicao@gmail.com>
2025-08-25 21:06:01 -0300

    Ta aí

--- cadeira_preto.py
+++ cadeira_preto.py
@@ -1,1 +1,17 @@
-python 
+
+
+#Em python o que é um cabeçalho do arquivo python,
+#para que ele serve, qual a diferença do cabeçalho
+#do arquivo para uma dockstring e para que serve uma dockstring
+
+#O cabeçalho de um arquivo Python é um bloco de comentários colocado no início do arquivo.
+#Ele não é obrigatório, mas é considerado uma boa prática, principalmente em projetos maiores.
+
+#As docstrings são mais comumente introduzidas no início de uma classe, de uma 
+#função ou no início do programa para definir o escopo do software ou o escopo de métodos,
+#seu símbolo padrão são as três aspas duplas ou simples.
+
+#Para quem está lendo o código do programa não existe muita diferença (exceto estética)
+#entre comentários e docstrings, mas para o usuário a diferença é marcante, pois uma docstring
+#não é ignorada pelo interpretador, mas passa a fazer parte do objeto sobre o qual ela foi definida,
+#podendo ser acessada através do help.
